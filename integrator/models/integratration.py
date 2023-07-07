7200a5b2cb398af81a92de66f04ebcb52810b360
Luis Aldama <luisaldama@icloud.com>
2023-07-07 10:23:34 -0400

    changed output dims to remove profile dims

--- integrator/models/integratration.py
+++ integrator/models/integratration.py
@@ -1,6 +1,6 @@
 from pylab import *
 import torch
-from integrator.layers import Linear,ResidualLayer
+from integrator.layers import Linear, ResidualLayer
 from integrator.models import MLP
 
 
@@ -16,47 +16,48 @@ class Integrator(torch.nn.Module):
         self.counts_std = torch.nn.Parameter(value, requires_grad=False)
 
     def get_intensity_sigma(self, xy, dxy, counts, mask=None, batch_size=100):
-        I,SigI = [],[]
+        I, SigI = [], []
         for batch in zip(
             torch.split(xy, batch_size, dim=0),
             torch.split(dxy, batch_size, dim=0),
             torch.split(counts, batch_size, dim=0),
             torch.split(mask, batch_size, dim=0),
-            ):
+        ):
             device = next(self.parameters()).device
-            i,s = self.get_intensity_sigma_batch(*(i.to(device=device) for i in batch))
+            i, s = self.get_intensity_sigma_batch(*(i.to(device=device) for i in batch))
             I.append(i.detach().cpu().numpy())
             SigI.append(s.detach().cpu().numpy())
-        I,SigI = np.concatenate(I), np.concatenate(SigI)
+        I, SigI = np.concatenate(I), np.concatenate(SigI)
         return I, SigI
 
     def get_per_spot_normalization(self, counts, mask=None):
         if mask is None:
-            return counts.mean(-1, keepdims=True) 
+            return counts.mean(-1, keepdims=True)
         return counts.sum(-1, keepdims=True) / mask.sum(-1, keepdims=True)
 
     def get_intensity_sigma_batch(self, xy, dxy, counts, mask=None):
         norm_factor = self.get_per_spot_normalization(counts, mask)
-        representation,p = self.encoder(xy, dxy, counts / norm_factor, mask)
+        representation, p = self.encoder(xy, dxy, counts / norm_factor, mask)
         params = self.profile.get_params(representation)
         q = self.profile.distribution(params)
-        I, SigI = q.mean,q.stddev
+        I, SigI = q.mean, q.stddev
         I, SigI = I * norm_factor, SigI * norm_factor
         return I, SigI
 
     def forward(self, xy, dxy, counts, mask=None, mc_samples=100):
         norm_factor = self.get_per_spot_normalization(counts, mask=mask)
-        representation,p = self.encoder(xy, dxy, counts / norm_factor, mask=mask)
-        profile, bg, q = self.profile(representation, dxy, mask=mask)
+        representation, p = self.encoder(xy, dxy, counts / norm_factor, mask=mask)
+        bg, q = self.profile(representation, dxy, mask=mask)
 
-        profile = profile * norm_factor
+        # profile = profile * norm_factor
         bg = bg * norm_factor
+        p = p * norm_factor
 
-        ll = self.likelihood(counts, profile,p, bg, q, mc_samples)
+        ll = self.likelihood(counts, profile, p, bg, q, mc_samples)
         if mask is None:
             nll = -ll.mean()
         else:
-            nll = -torch.where(mask, ll, 0.).sum() / mask.sum()
+            nll = -torch.where(mask, ll, 0.0).sum() / mask.sum()
 
         return nll
 
@@ -68,5 +69,3 @@ class Integrator(torch.nn.Module):
         ]
         norm = torch.cat(grads).norm()
         return norm
-
-
