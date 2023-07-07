0630696e596bf6e3f8ffa8ee1babff69829698a2
Luis Aldama <luisaldama@icloud.com>
2023-07-07 10:49:54 -0400

    dims

--- integrator/models/profile.py
+++ integrator/models/profile.py
@@ -10,7 +10,7 @@ class EllipticalProfileBase(torch.nn.Module):
         self.linear = Linear(
             dmodel,
             #  bg  cov dxy  I  SigI
-            3 + 1 + 1,
+            3 + 3 + 2 + 1 + 1,
         )
         self.eps = torch.nn.Parameter(data=torch.tensor(eps), requires_grad=False)
         self.beta = torch.nn.Parameter(data=torch.tensor(beta), requires_grad=False)
@@ -34,30 +34,30 @@ class EllipticalProfileBase(torch.nn.Module):
         ddxy = params[..., 6:8]
         return ddxy
 
-    # def profile(self, params, dxy):
-    # factory_kwargs = {
-    # "device": dxy.device,
-    # "dtype": dxy.dtype,
-    # }
-    # diag = self.constraint(params[..., 3:5])
-    # L = diag[:, None, :] * torch.eye(2, **factory_kwargs)[None, ...]
-    # L[..., 1, 0] = params[..., 5]
-    # precision = torch.matmul(L, L.transpose(-1, -2))
-    # ddxy = self.centroid_offset(params)
-    # X = dxy - ddxy
-    # profile = torch.exp(-X[..., None, :] @ precision @ X[..., :, None])
-    # profile = torch.squeeze(profile, [-2, -1])
-    # return profile
+    def profile(self, params, dxy):
+        factory_kwargs = {
+            "device": dxy.device,
+            "dtype": dxy.dtype,
+        }
+        diag = self.constraint(params[..., 3:5])
+        L = diag[:, None, :] * torch.eye(2, **factory_kwargs)[None, ...]
+        L[..., 1, 0] = params[..., 5]
+        precision = torch.matmul(L, L.transpose(-1, -2))
+        ddxy = self.centroid_offset(params)
+        X = dxy - ddxy
+        profile = torch.exp(-X[..., None, :] @ precision @ X[..., :, None])
+        profile = torch.squeeze(profile, [-2, -1])
+        return profile
 
     def get_params(self, representation):
         return self.linear(representation)
 
     def forward(self, representation, dxy, mask=None, mc_samples=10):
         params = self.get_params(representation)
-        # profile = self.profile(params, dxy)
+        profile = self.profile(params, dxy)
         bg = self.background(params, dxy)
         q = self.distribution(params)
-        return bg, q
+        return profile, bg, q
 
 
 class EllipticalProfile(EllipticalProfileBase):
