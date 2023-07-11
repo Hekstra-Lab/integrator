4cbeeac32c5f4d716d44a02f035bcd0062fc4f5e
Luis Aldama <luisaldama@icloud.com>
2023-07-11 11:26:38 -0400

    2023-07-11

--- integrator/models/profile.py
+++ integrator/models/profile.py
@@ -34,20 +34,20 @@ class EllipticalProfileBase(torch.nn.Module):
         ddxy = params[..., 6:8]
         return ddxy
 
-    # def profile(self, params, dxy):
-    # factory_kwargs = {
-    # "device": dxy.device,
-    # "dtype": dxy.dtype,
-    # }
-    # diag = self.constraint(params[..., 3:5])
-    # L = diag[:, None, :] * torch.eye(2, **factory_kwargs)[None, ...]
-    # L[..., 1, 0] = params[..., 5]
-    # precision = torch.matmul(L, L.transpose(-1, -2))
-    # ddxy = self.centroid_offset(params)
-    # X = dxy - ddxy
-    # profile = torch.exp(-X[..., None, :] @ precision @ X[..., :, None])
-    # profile = torch.squeeze(profile, [-2, -1])
-    # return profile
+    def profile(self, params, dxy):
+        factory_kwargs = {
+        "device": dxy.device,
+        "dtype": dxy.dtype,
+        }
+        diag = self.constraint(params[..., 3:5])
+        L = diag[:, None, :] * torch.eye(2, **factory_kwargs)[None, ...]
+        L[..., 1, 0] = params[..., 5]
+        precision = torch.matmul(L, L.transpose(-1, -2))
+        ddxy = self.centroid_offset(params)
+        X = dxy - ddxy
+        profile = torch.exp(-X[..., None, :] @ precision @ X[..., :, None])
+        profile = torch.squeeze(profile, [-2, -1])
+        return profile
 
     def get_params(self, representation):
         return self.linear(representation)
