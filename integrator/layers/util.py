e557aaf1ef922f8c6a7305d8eeb004344ed4aef7
Luis Aldama <luisaldama@icloud.com>
2023-12-17 16:54:30 -0500

    updated `RotationData()` class to return a single padded shoebox

--- integrator/layers/util.py
+++ integrator/layers/util.py
@@ -1,12 +1,11 @@
 import torch
 import math
 
-
+# Trunacated Normal
 def weight_initializer(weight):
     fan_avg = 0.5 * (weight.shape[-1] + weight.shape[-2])
-    std = math.sqrt(1. / fan_avg / 10.)
-    a = -2. * std
-    b =  2. * std
-    torch.nn.init.trunc_normal_(weight, 0., std, a, b)
+    std = math.sqrt(1.0 / fan_avg / 10.0)
+    a = -2.0 * std
+    b = 2.0 * std
+    torch.nn.init.trunc_normal_(weight, 0.0, std, a, b)
     return weight
-
