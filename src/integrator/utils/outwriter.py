a9050a9c3e1a63e405e1566f026d0ab134118849
Luis Aldama <luisaldama@icloud.com>
2024-10-02 10:24:53 -0400

    Outwriter now outputs three reflection files of different column combinations

--- src/integrator/utils/outwriter.py
+++ src/integrator/utils/outwriter.py
@@ -4,10 +4,19 @@ import numpy as np
 
 
 class OutWriter:
-    def __init__(self, output_dict, refl_file_name, out_file_name):
+    def __init__(
+        self,
+        output_dict,
+        refl_file_name,
+        out_file_name,
+        out_file_name2="nn_only.refl",
+        out_file_name3="dials_sum_nn_weak.refl",
+    ):
         self.output_dict = output_dict
         self.refl_file_name = refl_file_name
         self.out_file_name = out_file_name
+        self.out_file_name2 = out_file_name2
+        self.out_file_name3 = out_file_name3
 
     def write_output(self):
         train_res_df = pl.DataFrame(
@@ -25,22 +34,49 @@ class OutWriter:
             }
         )
 
+        # adding variance column
+        train_res_df = train_res_df.with_columns(
+            (pl.col("q_I_stddev") ** 2).alias("q_I_variance")
+        )
+        val_res_df = val_res_df.with_columns(
+            (pl.col("q_I_stddev") ** 2).alias("q_I_variance")
+        )
+
         res_df = pl.concat([train_res_df, val_res_df]).sort(pl.col("refl_id"))
         tbl = flex.reflection_table.from_file(self.refl_file_name)
         sel = np.asarray([False] * len(tbl))
         reflection_ids = res_df["refl_id"].cast(pl.Int32).to_list()
         intensity_preds = res_df["q_I_mean"].to_list()
-        intensity_stddev = res_df["q_I_stddev"].to_list()
-        intensity_variance = [x**2 for x in intensity_stddev]
+        intensity_variance = res_df["q_I_variance"].to_list()
 
         for id in reflection_ids:
             sel[id] = True
 
+        # Replaces both intensity.sum.value and intensity.sum.variance columns with our model's predictions
         temp = tbl.select(flex.bool(sel))
-        temp["intensity.sum.value"] = flex.double(intensity_preds)
 
-        temp["intensity.sum.variance"] = flex.double(intensity_variance)
-
+        # combination of dials.sum and network predictions
+        temp["intensity.prf.value"] = flex.double(intensity_preds)
+        temp["intensity.prf.variance"] = flex.double(intensity_variance)
         temp.as_file(self.out_file_name)
-        return
 
+        # network only predictions
+        temp["intensity.sum.value"] = flex.double(intensity_preds)
+        temp["intensity.sum.variance"] = flex.double(intensity_variance)
+        temp.as_file(self.out_file_name2)
+
+        # Replaces intensity.prf.value and intensity.prf.variance columns with intensity.sum.value
+        # and intensity.sum.variance
+        # Then, it replaces reflections used during training
+
+        indices = np.where(sel)
+        intensity_dials_sum = np.array(list(tbl["intensity.sum.value"]))
+        intensity_dials_variance = np.array(list(tbl["intensity.sum.variance"]))
+        intensity_dials_sum[indices] = intensity_preds
+        intensity_dials_variance[indices] = intensity_variance
+        temp2 = tbl
+        temp2["intensity.prf.value"] = flex.double(intensity_dials_sum)
+        temp2["intensity.prf.variance"] = flex.double(intensity_dials_variance)
+        temp2.as_file(self.out_file_name3)
+
+        return
