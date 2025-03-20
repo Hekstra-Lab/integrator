69a58d947e19ab6ee59b6865244491b02b85ced1
Luis Aldama <luisaldama@icloud.com>
2025-03-19 20:57:18 -0400

    2025-03-19

--- src/integrator/model/integrators/__init__.py
+++ src/integrator/model/integrators/__init__.py
@@ -1,5 +1,5 @@
 from .base_integrator import BaseIntegrator
-from .default_integrator import DefaultIntegrator, BernoulliIntegrator
+from .default_integrator import DefaultIntegrator
 from .dev_integrator import DevIntegrator
 from .mvn_integrator import MVNIntegrator
 from .unet_integrator import UNetIntegrator
