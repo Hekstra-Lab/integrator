9a76db7891e5b3d379e6772b0c1372fe7dc60fee
Luis Aldama <luisaldama@icloud.com>
2025-04-11 11:17:19 -0400

    2025-04-11

---
+++ src/integrator/model/distribution/half_normal.py
@@ -0,0 +1,40 @@
+import torch
+from integrator.layers import Linear, Constraint
+from torch.distributions import HalfNormal
+
+
+class HalfNormalDistribution(torch.nn.Module):
+    def __init__(
+        self,
+        dmodel,
+        constraint=Constraint(),
+        out_features=1,
+    ):
+        super().__init__()
+        self.fc = Linear(
+            in_features=dmodel,
+            out_features=out_features,
+        )
+        self.constraint = constraint
+        self.min_value = 1e-3
+        self.max_value = 100.0
+
+    def distribution(self, params):
+        scale = self.constraint(params)
+        return torch.distributions.half_normal.HalfNormal(scale)
+
+    def forward(self, representation):
+        params = self.fc(representation)
+        norm = self.distribution(params)
+        return norm
+
+
+if __name__ == "__main__":
+    # Example usage
+
+    dmodel = 128
+    half_normal_dist = HalfNormalDistribution(dmodel)
+
+    representation = torch.randn(10, dmodel)  # Example input
+
+    qbg = half_normal_dist(representation)
