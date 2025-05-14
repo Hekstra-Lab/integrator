cc638badff54659f758324c6b3ab0d54d4823e9c
Luis Aldama <luisaldama@icloud.com>
2025-05-14 08:06:20 -0400

    2025-05-14

--- src/integrator/model/integrators/base_integrator.py
+++ src/integrator/model/integrators/base_integrator.py
@@ -6,6 +6,7 @@ import pytorch_lightning as pl
 class BaseIntegrator(pl.LightningModule, ABC):
     def __init__(self):
         super().__init__()
+        self.train_loss = []
 
     @abstractmethod
     def forward(self, *args, **kwargs):
@@ -14,3 +15,8 @@ class BaseIntegrator(pl.LightningModule, ABC):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def on_train_epoch_end(self):
+        avg_loss = sum(self.train_loss) / len(self.train_loss)
+        self.log("train_loss", avg_loss)
+        self.train_loss = []
