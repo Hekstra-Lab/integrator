d11ac46ad23f15066d51d8a004395e1317ea277c
Luis Aldama <luisaldama@icloud.com>
2025-10-24 16:44:42 -0400

    2025-10-24

--- anomalous_table.py
+++ anomalous_table.py
@@ -1,13 +1,53 @@
 from pathlib import Path
 
+import plotly.graph_objects as go
 import polars as pl
 import pytextable
 
 from integrator.utils import BaseParser
 
 
+def plotly_table(
+    df: pl.DataFrame,
+    config: str,
+    id: str,
+):
+    # Create Plotly table for this method
+    header = df.columns
+    data = df.to_numpy().T
+
+    fig = go.Figure(
+        data=[
+            go.Table(
+                header=dict(
+                    values=header,
+                    line_color="darkslategray",
+                    fill_color="#CDCDCD",
+                    align="center",
+                    font=dict(color="black", size=14),
+                ),
+                cells=dict(
+                    values=[r for r in data],  # Transpose for Plotly format
+                    line_color="darkslategray",
+                    fill_color=[["white", "#F3F3F3"] * df.height],
+                    align="center",
+                    font=dict(color="black", size=12),
+                ),
+            )
+        ]
+    )
+
+    fig.update_layout(
+        title=f"Peaks heights for {config} (id:{id})",
+        margin=dict(l=20, r=20, t=60, b=20),
+        width=None,  # Full width
+        height=None,  # Auto height
+    )
+    return fig
+
+
 def main(args):
-    # loading arguments
+    # setting arguments
     root = Path(args.root)
     id = Path(args.wandb_id)
     config = args.config
@@ -33,21 +73,25 @@ def main(args):
         .sort("epoch")
     )
 
+    # constructing dataframe
     df = pl.concat(dfs, how="vertical")
     df_e = pl.DataFrame({"epoch": epochs})
-
     df = pl.concat([df_e, df], how="horizontal")
 
+    # save table as a LaTeX booktabs table
     data = df.to_numpy()
     header = tuple(df.columns)
 
-    # # save table as a LaTeX booktabs table
     pytextable.write(
         data,
-        f"{path}/table_{id}_{config}.text",
+        f"{path}/table_{id}_{config}.tex",
         header=header,
         caption=f"Peak heights for {config}",
     )
+
+    # write out a plotly image
+    fig = plotly_table(df, config, id.as_posix())
+    fig.write_image(f"{path}/peak_heights_{id}_{config}.png")
 
 
 if __name__ == "__main__":
