44cc911beacc4e970b1f3d340c30d13c471abe89
Luis Aldama <luisaldama@icloud.com>
2025-12-07 23:53:37 -0500

    2025-12-07

--- src/integrator/model/distributions/gamma.py
+++ src/integrator/model/distributions/gamma.py
@@ -59,50 +59,51 @@ class GammaDistribution(nn.Module):
         t = 0.5 * (t + 1.0)  # (0,1)
         return a + (b - a) * (t**2)  # square for large-range stability
 
-    # def forward(self, x) -> Gamma:
-    #     raw_k, raw_r = self.fc(x).chunk(2, dim=-1)
-    #     print("mean raw k", raw_k.mean())
-    #     print("min raw k", raw_k.min())
-    #     print("max raw k", raw_k.max())
-    #     print("mean raw r", raw_r.mean())
-    #     print("min raw r", raw_r.min())
-    #     print("max raw r", raw_r.max())
-    #
-    #     k = torch.nn.functional.softplus(raw_k) + 0.01
-    #     r = torch.nn.functional.softplus(raw_r) + 0.01
-    #
-    #     print("qbg,", self.fc.bias)
-    #
-    #     print("mean k", k.mean())
-    #     print("min k", k.min())
-    #     print("max k", k.max())
-    #     print("mean r", r.mean())
-    #     print("min r", r.min())
-    #     print("max r", r.max())
-    #
-    #     return Gamma(concentration=k.flatten(), rate=r.flatten())
-
     def forward(self, x) -> Gamma:
-        raw_m, raw_r = self.fc(x).chunk(2, dim=-1)
+        raw_k, raw_r = self.fc(x).chunk(2, dim=-1)
+        print("mean raw k", raw_k.mean())
+        print("min raw k", raw_k.min())
+        print("max raw k", raw_k.max())
+        print("mean raw r", raw_r.mean())
+        print("min raw r", raw_r.min())
+        print("max raw r", raw_r.max())
 
-        # bound log-mean and log-rate
-        log_mu = self.smooth_bound_square(
-            raw_m, self.log_mu_min, self.log_mu_max
-        )
-        log_r = self.smooth_bound_square(raw_r, self.log_r_min, self.log_r_max)
+        k = torch.nn.functional.softplus(raw_k) + 0.5
+        r = torch.nn.functional.softplus(raw_r) + 0.5
 
-        mu = torch.exp(log_mu)  # mean
-        r = torch.exp(log_r)  # rate
+        print("qbg,", self.fc.bias)
 
-        # shape
-        k = mu * r
-
-        # OPTIONAL: debug prints
-        # print("mu stats:", mu.min(), mu.max(), mu.mean())
-        # print("r stats:",  r.min(),  r.max(),  r.mean())
-        # print("fano stats:", (1.0 / r).min(), (1.0 / r).max())
+        print("mean k", k.mean())
+        print("min k", k.min())
+        print("max k", k.max())
+        print("mean r", r.mean())
+        print("min r", r.min())
+        print("max r", r.max())
 
         return Gamma(concentration=k.flatten(), rate=r.flatten())
+
+    # def forward(self, x) -> Gamma:
+    #     raw_m, raw_r = self.fc(x).chunk(2, dim=-1)
+    #
+    #     # bound log-mean and log-rate
+    #     log_mu = self.smooth_bound_square(
+    #         raw_m, self.log_mu_min, self.log_mu_max
+    #     )
+    #     log_r = self.smooth_bound_square(raw_r, self.log_r_min, self.log_r_max)
+    #
+    #     mu = torch.exp(log_mu)  # mean
+    #     r = torch.exp(log_r)  # rate
+    #
+    #     # shape
+    #     k = mu * r
+    #
+    #     # OPTIONAL: debug prints
+    #     # print("mu stats:", mu.min(), mu.max(), mu.mean())
+    #     # print("r stats:",  r.min(),  r.max(),  r.mean())
+    #     # print("fano stats:", (1.0 / r).min(), (1.0 / r).max())
+    #
+    #     return Gamma(concentration=k.flatten(), rate=r.flatten())
+    #
 
 
 if __name__ == "__main__":
