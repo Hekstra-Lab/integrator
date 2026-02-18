19b529b9cb660f90d4364643d65fe30121896597
Luis Aldama <luisaldama@icloud.com>
2026-02-17 21:54:05 -0500

    2026-02-17

--- src/integrator/model/loss/__init__.py
+++ src/integrator/model/loss/__init__.py
@@ -1,11 +1,7 @@
 from .base_loss import BaseLoss
-from .hierarchical_gamma_prior import HierarchicalGammaPrior
-from .hierarchical_loss import HierarchicalLoss
 from .loss import Loss
 
 __all__ = [
     "BaseLoss",
-    "HierarchicalGammaPrior",
-    "HierarchicalLoss",
     "Loss",
 ]
