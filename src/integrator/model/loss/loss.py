30cc2d746cd72358fddbde640d0ab15c215daaf4
Luis Aldama <luisaldama@icloud.com>
2026-03-10 12:43:06 -0400

    2026-03-10

--- src/integrator/model/loss/loss.py
+++ src/integrator/model/loss/loss.py
@@ -81,9 +81,9 @@ def _get_dirichlet_prior(
 
     if isinstance(conc, str):
         loaded = torch.load(conc)
-        loaded[loaded > 2] *= 40
-        n_components = loaded.numel()
-        loaded = loaded / loaded.sum() * n_components  # avg alpha = 1.0
+        # loaded[loaded > 2] *= 40 # NOTE: For testing
+        # n_components = loaded.numel()
+        # loaded = loaded / loaded.sum() * n_components  # avg alpha = 1.0
         return {"concentration": loaded.reshape(-1)}
 
     raise TypeError(f"Unsupported concentration type: {type(conc)}")
