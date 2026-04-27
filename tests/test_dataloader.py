41a86cc98579ba81d6b36bff94d4f2fd8bb31add
Luis Aldama <luisaldama@icloud.com>
2026-04-27 00:39:03 -0400

    updates

--- tests/test_dataloader.py
+++
@@ -1,6 +0,0 @@
-from integrator.utils import construct_data_loader, load_config
-
-path = "/Users/luis/temp/data/test_yaml.yaml"
-cfg = load_config(path)
-
-data_loader = construct_data_loader(cfg)
--- tests/test_distributions.py
+++
@@ -1,3 +0,0 @@
-# load configurations and dataset paths
-
-
